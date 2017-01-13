bd286d70b88106509cffc5a73a8ac501dd810605
Anand Tiwari <tiwarikanand@gmail.com>
2017-01-13 11:57:41 -0700

    Add hello-world and a package example

---
+++ anand/tiwari.go
@@ -0,0 +1,11 @@
+
+package anand
+
+import "fmt"
+
+func Anand_print() {
+	fmt.Printf("Anand says Hello, world.\n")
+	fmt.Println("Anand says Hello world with println");
+	fmt.Print("Andy says Hello work with print\n");
+}
+
